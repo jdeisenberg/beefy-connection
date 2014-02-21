b6eafd22a1b7589c7e9096ec0a71467b8d086caa
Clint Savage <herlo1@gmail.com>
2014-02-21 14:41:29 -0700

    managing GET and POST for user adding

--- bc.py
+++ bc.py
@@ -1,16 +1,41 @@
 import cherrypy
 
-class BeefyConnection (object):
+interests = {
+    'Ambassador': 'Represents Fedora',
+    'Packaging': 'Package Software to be included in Fedora',
+}
 
-    @cherrypy.expose
-    def index(self):
-        return "Welcome to the Beefy Connection"
+user = {}
 
-    def user(self):
-        return 'This is the "page" content'
-    page.exposed = True
+
+class BeefyUser:
+
+    exposed = True
+
+    def GET(self):
+
+        return('User Info:\n\nFirst name: {0}\nLast Name: {1}\nEmail: {2}'.format(user['first'], user['last'], user['email']))
+
+    def POST(self, first, last, email):
+
+        user = {
+            'first': first,
+            'last': last,
+            'email': email,
+        }
+
+        return ('Created a new user: {1}, {0}: {2}'.format(last, first, email))
 
 
 if __name__ == '__main__':
-    cherrypy.quickstart(BeefyConnection)
+
+    cherrypy.tree.mount(
+        BeefyUser(), '/api/user',
+        {'/':
+            {'request.dispatch': cherrypy.dispatch.MethodDispatcher()}
+        }
+    )
+
+    cherrypy.engine.start()
+    cherrypy.engine.block()
 
