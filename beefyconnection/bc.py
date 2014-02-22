4f51e1667415707cac188c9bd1b97a05cac3954f
Clint Savage <herlo1@gmail.com>
2014-02-22 00:35:05 -0700

    adding configs structure

--- beefyconnection/bc.py
+++ beefyconnection/bc.py
@@ -1,5 +1,4 @@
 import sqlite3
-import simplejson
 import ConfigParser
 
 class BeefyConfig(object):
@@ -36,6 +35,7 @@ class BeefyConfig(object):
 
 
 class BeefyConnection (object):
+
     exposed = True
 
     def index(self):
@@ -45,23 +45,39 @@ class BeefyConnection (object):
         return 'This is the "page" content'
 
 class BeefyDisplay (object):
-	def index(self):
-		template_data = BeefyConfig.cfgs['template']
-		exposed = True
-		from String import Template
-		f = open ('./template/beefy.template','r')
-		return Template(f.read(),dict())
+    def index(self):
+        template_data = BeefyConfig.cfgs['template']
+        exposed = True
+        from String import Template
+        f = open ('./template/beefy.template','r')
+        return Template(f.read(),dict())
 
 class BeefyUser(object):
 
     exposed = True
 
-    def POST(self, user_json):
-        """Get user_data json object from form submission"""
+    def __init__(self, bc):
+        self.conn = sqlite3.connect(bc.cfgs['db']['path'])
 
-        user_data = simplejson.loads(user_json)
+    def POST(self, first, last, email, organization='NULL', irc='NULL',
+        phone='NULL', address='NULL', city='NULL', state='NULL', postal='NULL',
+        language='NULL', other_interest='NULL', comments='NULL', interests=None):
+        """Get user data object from form submission"""
 
-        return ('User Data: {0}'.format(user_data))
+        self.conn.execute(
+            '''INSERT INTO person ({0}, {1}, {2}, {3}, {4}, {5}, {6}, {7}, {8},
+               {9}, {10}, {11}, {12}, {13})'''.format(last, first, email,
+               organization, irc, phone, address, city, state, postal, language,
+               other_interest, comments))
+
+        ## getuid here
+
+        if (interests):
+            interest_list = interests.split()
+            for interest in interest_lists:
+              self.conn.execute('INSERT INTO interests (uid, interest)')
+
+        return ('Created a new user: {1}, {0}: {2}'.format(last, first, email))
 
 class BeefyPic(object):
 
