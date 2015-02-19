0269bf873f7b4bf2c763670e4b4a0ab2f35fa163
Joshua Santos <nerdsville@nerdsville.net>
2015-02-19 08:21:57 -0800

    Adding a sample person and returning the result from read

--- beefyconnection/beefyflask.py
+++ beefyconnection/beefyflask.py
@@ -1,4 +1,5 @@
 from flask import *
+from beefy_db import BeefyDatabase
 app = Flask(__name__)
 
 def globals():
@@ -11,8 +12,37 @@ def form():
 
 @app.route("/bc-post",methods=['POST'])
 def post():
-    return str(request.form)
+    databaseConnector = new BeefyDatabase("sqlite3://person.db")
+    databaseConnector.addPerson(first_name="Joshua",
+                                last_name="Santos",
+                                phone="9094360697",
+                                city="Fontana",
+                                state="California",
+                                postal_code="92336",
+                                irc="nerdsville",
+                                fb="nerdsville",
+                                twitter="nerdsvillellc",
+                                interests="tennis, programming, math",
+                                email="nerdsville@nerdsville.net",
+                                fas="nerdsville")
+    return databaseConnector.readPerson()
 
+"""
+    id=Column(Integer, primary_key=True)
+    first_name=Column(String)
+    last_name=Column(String)
+    phone=Column(String)
+    city=Column(String)
+    state=Column(String)
+    postal_code=Column(String)
+    irc=Column(String)
+    fb=Column(String)
+    twitter=Column(String)
+    interests=Column(String)
+    email=Column(String)
+    fas=Column(String)
+
+"""
 @app.route("/bc-success")
 def success():
     return render_template('thanks.html',**globals())
