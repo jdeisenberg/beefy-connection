4ce906dbe1c37e011d84ed74bc9f43f57a6a2696
Scott A. Williams <sawilliams@cmc.edu>
2015-02-19 14:37:37 -0800

    Template tweaks

--- beefyconnection/beefyflask.py
+++ beefyconnection/beefyflask.py
@@ -1,10 +1,13 @@
 from flask import Flask,render_template
 app = Flask(__name__)
 
+def globals():
+    return { 'title' : 'Beefy Connection!',
+             'logo'  : '/static/images/beefy.png'}
+
 @app.route("/")
 def form():
-    data = { 'title' : 'Beefy Connection'}
-    return render_template('mainform.html',**data)
+    return render_template('mainform.html',**globals())
 
 if __name__ == "__main__":
     app.debug = True
